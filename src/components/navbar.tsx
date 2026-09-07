5771fa7fec9b2180120fff44467700eec8f3c95f
MiroDev20 <mirodev20@gmail.com>
2026-09-07 14:58:32 -0500

    🔧 add Navbar component structure

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <img src="./src/assets/icons/icon-close.svg" alt="close navbar"/>
+                <li>
+                    <a href="#">Collections</a>
+                </li>
+                <li>
+                    <a href="#">Men</a>
+                </li>
+                <li>
+                    <a href="#">Women</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
