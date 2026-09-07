b93561a879ae2d3f4791739bf0a3bebeea5cc277
MiroDev20 <mirodev20@gmail.com>
2026-09-07 16:32:04 -0500

    🎨 add base styles to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,53 @@
 export const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <img src="./src/assets/icons/icon-close.svg" alt="close navbar"/>
-                <li>
-                    <a href="#">Collections</a>
-                </li>
-                <li>
-                    <a href="#">Men</a>
-                </li>
-                <li>
-                    <a href="#">Women</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        <div className="
+                absolute
+                w-full
+                h-screen
+                bg-black/30
+                
+            "
+        >
+            <nav className="
+                    flex
+                    flex-col
+                    p-4
+                    gap-8
+                    w-50
+                    h-screen
+                    bg-white
+                "
+            >
+                <img className="
+                        w-3
+                    "
+                    src="./src/assets/icons/icon-close.svg"
+                    alt="close navbar"
+                />
+                <ul className="
+                        flex
+                        flex-col
+                        gap-4
+                        font-bold
+                    "
+                >
+                    <li>
+                        <a href="#">Collections</a>
+                    </li>
+                    <li>
+                        <a href="#">Men</a>
+                    </li>
+                    <li>
+                        <a href="#">Women</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
