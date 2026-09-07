ec395e2c55cad1fa5196083c3121883f39a454bb
MiroDev20 <mirodev20@gmail.com>
2026-09-07 16:40:16 -0500

    🧪 render Navbar in App for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import {Navbar} from "./components/navbar.tsx"
+
 function App() {
   return (
-    <>
-    </>
+    <main className="
+        min-h-screen
+        bg-orange
+      "
+    >
+      <Navbar/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus veritatis corrupti libero praesentium laudantium ducimus veniam magni sint tempore inventore numquam, totam repellat est eum eos facere. Non, animi reiciendis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus veritatis corrupti libero praesentium laudantium ducimus veniam magni sint tempore inventore numquam, totam repellat est eum eos facere. Non, animi reiciendis.</p>
+    </main>
   )
 }
 
